13b40d82db31a79c2d0aaaeb3c24a9783c6e904a
aliahmad866 <43981395+aliahmad866@users.noreply.github.com>
2019-03-11 15:09:39 +0500

    pages

---
+++ SignUp.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace PlagiarismChecker
+{
+    public partial class SignUp : ContentPage
+    {
+        public SignUp()
+        {
+            InitializeComponent();
+        }
+    }
+}
